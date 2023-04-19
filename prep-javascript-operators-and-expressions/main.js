b1b6b24b405dd51d0a669a2ada1095dedfe6bd2a
Bug Smith <salttyyy@gmail.com>
2023-04-19 04:55:46 +0000

    prep-javascript-operators-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,41 @@
+const number1 = 3;
+const number2 = 4;
+const product = number1 * number2;
+
+let charge = 5;
+const payment = 2;
+const amountRemaining = (charge -= payment);
+
+const tests = 10;
+const assignments = 30;
+const final = 60;
+const grade = (tests + assignments + final) / 3;
+
+const firstName = 'Bug';
+const lastName = 'Smith';
+const fullName = firstName + ' ' + lastName;
+
+const pH1 = 9 > 7;
+const pH = pH1;
+const isAcidic = pH;
+
+const numberOfSoliders = 299 === 300;
+const isSparta = numberOfSoliders;
+
+console.log(product);
+console.log(typeof product);
+
+console.log(amountRemaining);
+console.log(typeof amountRemaining);
+
+console.log(grade);
+console.log(typeof grade);
+
+console.log(fullName);
+console.log(typeof fullName);
+
+console.log(isAcidic);
+console.log(typeof isAcidic);
+
+console.log(isSparta);
+console.log(typeof isSparta);
