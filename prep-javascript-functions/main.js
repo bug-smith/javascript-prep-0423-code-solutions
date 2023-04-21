7376dafbebb503b860d4bb550ae4bb6b3838c9de
Bug Smith <salttyyy@gmail.com>
2023-04-21 16:00:50 +0000

    prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,65 @@
+function addTwoNumbers(num1, num2) {
+  var combinedNumber = num1 + num2;
+  return combinedNumber;
+}
+
+addTwoNumbers();
+
+function convertHoursToMinutes(hours) {
+  var totalMinutes = hours * 60;
+  return totalMinutes;
+}
+
+convertHoursToMinutes();
+
+function getGreeting(nickname) {
+  var greeting = 'Hello, ' + nickname + '!';
+  return greeting;
+}
+
+getGreeting();
+
+function addAndMultiplyBy5(num1, num2) {
+  var summedNumbers = num1 + num2;
+  var totalNumbers = summedNumbers * 5;
+  return totalNumbers;
+}
+
+addAndMultiplyBy5();
+
+function multiplyAndDivideBy5(num1, num2) {
+  var multipliedNumber = num1 * num2;
+  const totalNumber = multipliedNumber / 5;
+  return totalNumber;
+}
+
+multiplyAndDivideBy5();
+
+function subtractTwoNumbers(num1, num2) {
+  var subtractedNumbers = num1 - num2;
+  return subtractedNumbers;
+}
+
+subtractTwoNumbers();
+
+function getCircleCircumference(radius) {
+  var totalDiameter = radius * 2;
+  const totalRadius = totalDiameter * Math.PI;
+  return totalRadius;
+}
+
+getCircleCircumference();
+
+function getFullName(firstName, lastName) {
+  var fullName = firstName + ' ' + lastName;
+  return fullName;
+}
+
+getFullName();
+
+function cube(number) {
+  var cubedNumber = number * number * number;
+  return cubedNumber;
+}
+
+cube();
